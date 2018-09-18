1a4e126f632ecc564927ebc916544f5971ef0a76
Piyush Mishra <piyushmishra1999@rediffmail.com>
2018-09-18 19:44:05 +0530

    Portfolio

---
+++ javascript.js
@@ -0,0 +1,51 @@
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY >= 100) nav.className = 'navbar navbar-expand-md  navbar-dark bg-dark fixed-top'; else nav.className = 'navbar navbar-expand-md  navbar-dark trans';
+};
+
+
+
+$("#homepage").on("click",function(){
+        $('html,body').animate({
+        scrollTop: $(".homepage").offset().top},
+        'slow');
+})
+$("#aboutus").on("click",function(){
+        $('html,body').animate({
+        scrollTop: $("#section2").offset().top},
+        'slow');
+})
+$("#services").on("click",function(){
+        $('html,body').animate({
+        scrollTop: $("#section3").offset().top},
+        'slow');
+})
+$("#contact").on("click",function(){
+        $('html,body').animate({
+        scrollTop: $(".contactform").offset().top},
+        'slow');
+})
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".homepage").offset().top},"1000");return false})})
+
+
+
+var text = ["<h1>Hello!</h1>","<h1>I am Piyush.  </h1>","<h1>Welcome to My Portfolio.   </h1>"];
+var counter = 0;
+var elem = document.querySelector(".typewriter");
+var inst = setInterval(change,3500);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
